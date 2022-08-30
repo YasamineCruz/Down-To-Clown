258a015787b2aaa3cb506ce6b4b5a0d3bf61ab57
YasamineCruz <yasamine.97.cruz@gmail.com>
2022-08-30 15:59:47 -0700

    Get all users done

---
+++ backend/routes/api/groups.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User, Group, Membership, sequelize, GroupImage } = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+
+router.get('/', async(req, res, next) => {
+
+    // const { count, rows } = await Group.findAll({
+    //     include: [ {model: User, attributes: []}, { model: GroupImage, as: "Preview Image"}]
+    // });
+    //   console.log(count);
+    //   console.log(rows);
+    let groups = await Group.findAll({
+        include: [
+                 { model: User, attributes: [] }, 
+                 { model: GroupImage, as: "Preview Image" } 
+                ],
+        attributes: {
+            include: [
+                [
+                    sequelize.fn("COUNT", sequelize.col("User.id")), 
+                    "numMembers"
+                ]
+             ]
+            },
+        group: ['Group.id']
+    })
+
+    // let groups = rows
+    if(groups){
+        res.json(groups)
+    } else{
+        const err = new Error('No Groups found');
+        err.status = 401;
+        err.title = 'No Groups';
+        err.errors = ['There are no groups'];
+        return next(err);
+    }
+})
+
+module.exports = router;
