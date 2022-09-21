e1df4ede89623f291d9e2e8e8acd531dd6903d1e
Yasamine <yasamine.97.cruz@gmail.com>
2022-09-21 16:35:17 -0700

    Modal for groups deleted decided to do a page instead

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
 import { GroupProvider } from './context/GroupContext';
 
+
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
